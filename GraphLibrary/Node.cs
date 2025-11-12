b3ac397d9c8f054558a304cd750dc33f192b6f25
Jesse Harlan <jesse.harlan@centralia.edu>
2025-11-12 13:49:44 -0800

    Add project files.

---
+++ GraphLibrary/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphLibrary
+{
+    public class Node
+    {
+        // what the node represents
+        public string Value { get; set; }
+        public List<Node> Children { get; set; }
+
+        public Node(string value)
+        {
+            Value = value;
+            Children = new List<Node>();
+        }
+
+        public Node AddChild(Node node)
+        {
+            Node obj = node;
+            Children.Add(node);
+            return obj;
+        }
+    }
+}
---
+++ GraphLibraryTestbed/Program.cs
@@ -0,0 +1,56 @@
+﻿using GraphLibrary;
+using System.ComponentModel.Design;
+
+namespace GraphLibraryTestbed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tree myTree = new Tree();
+
+            //myTree.Print();
+
+
+            Node root = new Node("C:/");
+            
+
+            Node users = root.AddChild(new Node("Users"));
+            users.AddChild(new Node("Jesse"));
+            users.AddChild(new Node("Tom"));
+            users.AddChild(new Node("Alice"));
+            Node programs = root.AddChild(new Node("Programs"));
+            Node code = programs.AddChild(new Node("VSCode"));
+
+            code.AddChild(new Node("bin"));
+
+            PrintTree(root, 0);
+        }
+        
+        static void PrintTree(Node root, int level)
+        {
+            // base case
+            if (root == null)
+            {
+                return;
+            }
+
+            string line = "";
+
+            for (int i = 0; i < level; i++)
+            {
+                line += "  ";
+            }
+
+            line += root.Value;
+
+            Console.WriteLine(line);
+
+            // recurse
+            for (int i = 0; i < root.Children.Count; i++)
+            {
+                PrintTree(root.Children[i], level + 1);
+            }
+        }
+    }
+}
